fef9dc44fc8f104936d4f559f7a4d86da06b714b
narongsaksw <earth.narongsak@gmail.com>
2022-10-23 18:32:19 +0700

    example playground

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { Color } from "@ds.e/react";
+import { Text, Margin } from "@ds.e/react";
 
 import "@ds.e/scss/lib/Utilities.css";
+import "@ds.e/scss/lib/Text.css";
+import "@ds.e/scss/lib/Margin.css";
+import "@ds.e/scss/lib/global.css";
 
 const container = document.querySelector("#root");
 const root = createRoot(container!);
 
-root.render(<Color hexCode="#000" />);
+root.render(<div>
+    <Margin left top space="sm">
+        <Text size="xs">this is text</Text>
+    </Margin>
+</div>);
