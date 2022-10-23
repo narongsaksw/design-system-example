4a1a318287d9f95ed0be13f194e92fcf7c4e2bf8
narongsaksw <earth.narongsak@gmail.com>
2022-10-23 18:35:45 +0700

    delete packages/react/lib

--- packages/react/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Color from './atoms/Color';
-import Text from './atoms/Text';
-export { Color, Text };
--- packages/react/lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Color } from './atoms/Color/Color.js';
-export { default as Text } from './atoms/Text/Text.js';
-//# sourceMappingURL=index.js.map
